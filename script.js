75bce98f8fd35226b68dcacc02e8cb9f890d4b45
Amin Elmegy <54503748+aminmlegy@users.noreply.github.com>
2022-03-22 16:49:24 +0200

    updated

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 let Form = document.querySelector("form"),
   inputText = document.querySelector(".text"),
   listItems = document.querySelector(".list");
+
 class Todo {
   constructor() {
     this.arr = JSON.parse(localStorage.getItem("items")) || [];
-    this.ifff;
+    this.condations = "show";
   }
-  add(idTodo, todoVal) {
-    this.arr.push({ idTodo: idTodo, todoVal: todoVal });
+
+  add(id, todoVal) {
+    this.arr.push({ idTodo: id, todoVal: todoVal });
     localStorage.setItem("items", JSON.stringify(this.arr));
   }
+
   showItems() {
     listItems.innerHTML = "";
     this.arr.forEach((element) => {
@@ -26,6 +29,7 @@ class Todo {
         `;
     });
   }
+
   deleteItem(id) {
     let NewLocal = this.arr.filter((item) => {
       return item.idTodo !== +id;
@@ -33,28 +37,39 @@ class Todo {
     localStorage.setItem("items", JSON.stringify(NewLocal));
     this.arr = JSON.parse(localStorage.getItem("items"));
   }
-  // edit(id) {
-  //   let findItem = { idTodo: id, todoVal: inputText.value.trim() };
-  //   inputText.value = findItem.todoVal;
-  //   let editItem = this.arr.map((item) => {
-  //     return item.idTodo === findItem.idTodo ? findItem : item;
-  //   });
-  //   localStorage.setItem("items", JSON.stringify(editItem));
-  //   this.arr = JSON.parse(localStorage.getItem("items"));
-  // }
+
+  getItem(id) {
+    this.condations = +id;
+    let findd = this.arr.find((item) => {
+      return item.idTodo === this.condations;
+    });
+    inputText.value = findd.todoVal;
+  }
+
+  editItem(val) {
+    let itemEdit = { idTodo: this.condations, todoVal: val };
+    let findd = this.arr.map((item) => {
+      return item.idTodo === itemEdit.idTodo ? itemEdit : item;
+    });
+    localStorage.setItem("items", JSON.stringify(findd));
+    this.arr = JSON.parse(localStorage.getItem("items"));
+    this.condations = "show";
+  }
 }
 
 let appTodo = new Todo();
 
-if (appTodo.arr.length) {
-  appTodo.showItems();
-}
+appTodo.showItems();
 
 Form.addEventListener("submit", (e) => {
   e.preventDefault();
-  let idRandoum = Math.floor(Math.random() * 1000000);
-  if (inputText.value !== "") {
-    appTodo.add(idRandoum, inputText.value.trim());
+  if (appTodo.condations === "show") {
+    let idRandoum = Math.floor(Math.random() * 100000);
+    if (inputText.value.trim() !== "") {
+      appTodo.add(idRandoum, inputText.value.trim());
+    }
+  } else {
+    appTodo.editItem(inputText.value.trim());
   }
   appTodo.showItems();
   inputText.value = "";
@@ -64,9 +79,7 @@ document.addEventListener("click", (e) => {
   if (e.target.className === "delete") {
     appTodo.deleteItem(e.target.dataset.id);
     e.target.parentElement.parentElement.remove();
+  } else if (e.target.className === "edit") {
+    appTodo.getItem(e.target.dataset.edit);
   }
-  // else if (e.target.className === "edit") {
-  //   appTodo.ifff = e.target.dataset.edit;
-  //   appTodo.edit(e.target.dataset.edit);
-  // }
 });
