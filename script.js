d818ecdf5c22da051e7d7a82af8c0c597b8d1b32
Amin Elmegy <54503748+aminmlegy@users.noreply.github.com>
2022-03-27 17:25:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,84 +2,158 @@ let Form = document.querySelector("form"),
   inputText = document.querySelector(".text"),
   listItems = document.querySelector(".list");
 
-class Todo {
-  constructor() {
-    this.arr = JSON.parse(localStorage.getItem("items")) || [];
-    this.condations = "show";
-  }
+// class Todo {
+//   constructor() {
+//     this.arr = JSON.parse(localStorage.getItem("items")) || [];
+//     this.condations = "show";
+//   }
 
-  add(id, todoVal) {
-    this.arr.push({ idTodo: id, todoVal: todoVal });
-    localStorage.setItem("items", JSON.stringify(this.arr));
-  }
+//   add(id, todoVal) {
+//     this.arr.push({ idTodo: id, todoVal: todoVal });
+//     localStorage.setItem("items", JSON.stringify(this.arr));
+//   }
 
-  showItems() {
+//   showItems() {
+//     listItems.innerHTML = "";
+//     this.arr.forEach((element) => {
+//       listItems.innerHTML += `
+//         <div class="item">
+//         <div class="text">
+//             ${element.todoVal}
+//         </div>
+//         <div class="option">
+//             <span class="delete" data-id="${element.idTodo}">&cross;</span>
+//             <span class="edit" data-edit="${element.idTodo}">&orarr;</span>
+//         </div>
+//       </div>
+//         `;
+//     });
+//   }
+
+//   deleteItem(id) {
+//     this.arr = this.arr.filter((item) => {
+//       return item.idTodo !== +id;
+//     });
+//     localStorage.setItem("items", JSON.stringify(this.arr));
+//   }
+
+//   getItem(id) {
+//     this.condations = +id;
+//     let findd = this.arr.find((item) => {
+//       return item.idTodo === this.condations;
+//     });
+//     inputText.value = findd.todoVal;
+//   }
+
+//   editItem(val) {
+//     let itemEdit = { idTodo: this.condations, todoVal: val };
+//     this.arr = this.arr.map((item) => {
+//       return item.idTodo === itemEdit.idTodo ? itemEdit : item;
+//     });
+//     localStorage.setItem("items", JSON.stringify(this.arr));
+//     this.condations = "show";
+//   }
+// }
+
+// let appTodo = new Todo();
+
+// appTodo.showItems();
+
+// Form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   if (appTodo.condations === "show") {
+//     let idRandoum = Math.floor(Math.random() * 100000);
+//     if (inputText.value.trim() !== "") {
+//       appTodo.add(idRandoum, inputText.value.trim());
+//     }
+//   } else {
+//     appTodo.editItem(inputText.value.trim());
+//   }
+//   appTodo.showItems();
+//   inputText.value = "";
+// });
+
+// document.addEventListener("click", (e) => {
+//   if (e.target.className === "delete") {
+//     appTodo.deleteItem(e.target.dataset.id);
+//     e.target.parentElement.parentElement.remove();
+//   } else if (e.target.className === "edit") {
+//     appTodo.getItem(e.target.dataset.edit);
+//   }
+// });
+const Wrap = (function () {
+  let arr = JSON.parse(localStorage.getItem("items")) || [];
+  let condations = "show";
+  function add(id, todoVal) {
+    arr.push({ idTodo: id, todoVal: todoVal });
+    localStorage.setItem("items", JSON.stringify(arr));
+  }
+  function showItems() {
     listItems.innerHTML = "";
-    this.arr.forEach((element) => {
+    arr.forEach((element) => {
       listItems.innerHTML += `
-        <div class="item">
-        <div class="text">
-            ${element.todoVal}
+          <div class="item">
+          <div class="text">
+              ${element.todoVal}
+          </div>
+          <div class="option">
+              <span class="delete" data-id="${element.idTodo}">&cross;</span>
+              <span class="edit" data-edit="${element.idTodo}">&orarr;</span>
+          </div>
         </div>
-        <div class="option">
-            <span class="delete" data-id="${element.idTodo}">&cross;</span>
-            <span class="edit" data-edit="${element.idTodo}">&orarr;</span>
-        </div>
-      </div>
-        `;
+          `;
     });
   }
-
-  deleteItem(id) {
-    let NewLocal = this.arr.filter((item) => {
+  function deleteItem(id) {
+    arr = arr.filter((item) => {
       return item.idTodo !== +id;
     });
-    localStorage.setItem("items", JSON.stringify(NewLocal));
-    this.arr = JSON.parse(localStorage.getItem("items"));
+    localStorage.setItem("items", JSON.stringify(arr));
   }
-
-  getItem(id) {
-    this.condations = +id;
-    let findd = this.arr.find((item) => {
-      return item.idTodo === this.condations;
+  function getItem(id) {
+    condations = +id;
+    let findd = arr.find((item) => {
+      return item.idTodo === condations;
     });
     inputText.value = findd.todoVal;
   }
-
-  editItem(val) {
-    let itemEdit = { idTodo: this.condations, todoVal: val };
-    let findd = this.arr.map((item) => {
+  function editItem(val, cond) {
+    let itemEdit = { idTodo: cond, todoVal: val };
+    arr = arr.map((item) => {
       return item.idTodo === itemEdit.idTodo ? itemEdit : item;
     });
-    localStorage.setItem("items", JSON.stringify(findd));
-    this.arr = JSON.parse(localStorage.getItem("items"));
-    this.condations = "show";
+    localStorage.setItem("items", JSON.stringify(arr));
+    condations = "show";
   }
-}
-
-let appTodo = new Todo();
-
-appTodo.showItems();
-
+  return {
+    condations,
+    showItems,
+    add,
+    editItem,
+    deleteItem,
+    getItem,
+  };
+})();
+Wrap.showItems();
 Form.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (appTodo.condations === "show") {
+  if (Wrap.condations === "show") {
     let idRandoum = Math.floor(Math.random() * 100000);
     if (inputText.value.trim() !== "") {
-      appTodo.add(idRandoum, inputText.value.trim());
+      Wrap.add(idRandoum, inputText.value.trim());
     }
-  } else {
-    appTodo.editItem(inputText.value.trim());
+  } else if (!Wrap.condations === "show") {
+    Wrap.editItem(inputText.value.trim(), Wrap.condations);
   }
-  appTodo.showItems();
+  Wrap.showItems();
   inputText.value = "";
 });
 
 document.addEventListener("click", (e) => {
   if (e.target.className === "delete") {
-    appTodo.deleteItem(e.target.dataset.id);
+    Wrap.deleteItem(e.target.dataset.id);
     e.target.parentElement.parentElement.remove();
   } else if (e.target.className === "edit") {
-    appTodo.getItem(e.target.dataset.edit);
+    Wrap.getItem(e.target.dataset.edit);
   }
 });
