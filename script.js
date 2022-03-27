1d07e4e98533e17dd487adab88a1f18cd5bce3b5
Amin Elmegy <54503748+aminmlegy@users.noreply.github.com>
2022-03-27 20:58:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,11 @@ let Form = document.querySelector("form"),
 //     this.condations = "show";
 //   }
 
-//   add(id, todoVal) {
-//     this.arr.push({ idTodo: id, todoVal: todoVal });
+//   add(todoVal) {
+//     let idRandoum = Date.now();
+//     this.arr.push({ idTodo: idRandoum, todoVal: todoVal });
 //     localStorage.setItem("items", JSON.stringify(this.arr));
+//     this.showItems();
 //   }
 
 //   showItems() {
@@ -52,6 +54,7 @@ let Form = document.querySelector("form"),
 //     });
 //     localStorage.setItem("items", JSON.stringify(this.arr));
 //     this.condations = "show";
+//     this.showItems();
 //   }
 // }
 
@@ -62,14 +65,12 @@ let Form = document.querySelector("form"),
 // Form.addEventListener("submit", (e) => {
 //   e.preventDefault();
 //   if (appTodo.condations === "show") {
-//     let idRandoum = Math.floor(Math.random() * 100000);
 //     if (inputText.value.trim() !== "") {
-//       appTodo.add(idRandoum, inputText.value.trim());
+//       appTodo.add(inputText.value.trim());
 //     }
 //   } else {
 //     appTodo.editItem(inputText.value.trim());
 //   }
-//   appTodo.showItems();
 //   inputText.value = "";
 // });
 
@@ -81,13 +82,18 @@ let Form = document.querySelector("form"),
 //     appTodo.getItem(e.target.dataset.edit);
 //   }
 // });
+
 const Wrap = (function () {
   let arr = JSON.parse(localStorage.getItem("items")) || [];
   let condations = "show";
+  showItems();
+
   function add(id, todoVal) {
     arr.push({ idTodo: id, todoVal: todoVal });
     localStorage.setItem("items", JSON.stringify(arr));
+    showItems();
   }
+
   function showItems() {
     listItems.innerHTML = "";
     arr.forEach((element) => {
@@ -104,56 +110,63 @@ const Wrap = (function () {
           `;
     });
   }
+
   function deleteItem(id) {
     arr = arr.filter((item) => {
       return item.idTodo !== +id;
     });
     localStorage.setItem("items", JSON.stringify(arr));
   }
+
   function getItem(id) {
-    condations = +id;
+    condations = id;
     let findd = arr.find((item) => {
       return item.idTodo === condations;
     });
     inputText.value = findd.todoVal;
   }
-  function editItem(val, cond) {
-    let itemEdit = { idTodo: cond, todoVal: val };
+
+  function editItem(val) {
+    let itemEdit = { idTodo: condations, todoVal: val };
     arr = arr.map((item) => {
-      return item.idTodo === itemEdit.idTodo ? itemEdit : item;
+      return item.idTodo == itemEdit.idTodo ? itemEdit : item;
     });
+    console.log(arr);
     localStorage.setItem("items", JSON.stringify(arr));
     condations = "show";
+    showItems();
+  }
+  function usedd() {
+    if (condations === "show") {
+      let idRandoum = Math.floor(Math.random() * 100000);
+
+      if (inputText.value.trim() !== "") {
+        add(idRandoum, inputText.value.trim());
+      }
+    } else {
+      editItem(inputText.value.trim());
+    }
   }
   return {
-    condations,
-    showItems,
-    add,
-    editItem,
     deleteItem,
     getItem,
+    usedd,
   };
 })();
-Wrap.showItems();
+
 Form.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (Wrap.condations === "show") {
-    let idRandoum = Math.floor(Math.random() * 100000);
-    if (inputText.value.trim() !== "") {
-      Wrap.add(idRandoum, inputText.value.trim());
-    }
-  } else if (!Wrap.condations === "show") {
-    Wrap.editItem(inputText.value.trim(), Wrap.condations);
-  }
-  Wrap.showItems();
+  Wrap.usedd();
   inputText.value = "";
 });
 
 document.addEventListener("click", (e) => {
   if (e.target.className === "delete") {
     Wrap.deleteItem(e.target.dataset.id);
+
     e.target.parentElement.parentElement.remove();
   } else if (e.target.className === "edit") {
-    Wrap.getItem(e.target.dataset.edit);
+    const editId = +e.target.dataset.edit;
+    Wrap.getItem(editId);
   }
 });
